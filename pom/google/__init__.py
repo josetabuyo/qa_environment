d62a227080a1048baf8e9f629669a613ba633961
José Tabuyo <josetabuyo@gmail.com>
2024-05-12 20:33:25 -0300

    Removed copy pasta issue

--- pom/google/__init__.py
+++ pom/google/__init__.py
@@ -12,11 +12,4 @@ class GooglePage:
         search_box = self.driver.find(By.NAME, "q")
         search_box.send_keys(terms)
         search_box.submit()
-
-    class Results:
-        def __init__(self, driver):
-            self.driver = driver
-        
-        def get_result_stats(self):
-            return self.driver.find_element(By.ID, "result-stats")
-        
+    
